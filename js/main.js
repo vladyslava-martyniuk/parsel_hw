c088a1961a76f4cc709a8b158216e62675b9c246
vladyslava-martyniuk <vlada.martyniuk@gmail.com>
2025-02-21 11:23:34 +0200

    hw done

--- js/main.js
+++ js/main.js
@@ -1,28 +1,25 @@
-// index.js
-document.getElementById('feedbackForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Зупиняємо дефолтну поведінку форми
 
-    // Отримуємо значення полів форми
+document.getElementById('feedbackForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let message = document.getElementById('message').value;
 
-    // Створюємо об'єкт з даними форми
+ 
     let formData = {
         name: name,
         email: email,
         message: message
     };
 
-    // Демонстрація успішного відправлення форми
+   
     let successMessage = document.getElementById('successMessage');
     successMessage.classList.remove('hidden');
 
-    // Видалення повідомлення про успішність через 3 секунди
     setTimeout(function() {
         successMessage.classList.add('hidden');
     }, 3000);
 
-    // Очищення полів форми
+
     document.getElementById('feedbackForm').reset();
 });
